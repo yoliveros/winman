fe8c89b7c8f5fd34906cff686f562ad80b863abc
yoliveros <yoelvisoliveros@gmail.com>
2024-04-15 23:37:23 -0400

    feat: added window.h

--- src/main.zig
+++ src/main.zig
@@ -3,7 +3,24 @@ const windows = @cImport({
     @cInclude("windows.h");
 });
 
+fn callback(hwnd: windows.HWND, lparam: windows.LPARAM) callconv(.C) windows.BOOL {
+    _ = lparam;
+    const length = windows.GetWindowTextLength(hwnd);
+    var gpa = std.heap.GeneralPurposeAllocator();
+    defer gpa.deinit();
+    const allocator = gpa.allocator();
+
+    const buffer = try allocator.alloc(u8, length + 1);
+    defer allocator.free(buffer);
+    const windowTitle = @as([]const u8, buffer[0..length]);
+
+    std.log.info("{s}\n", .{windowTitle});
+
+    return windows.TRUE;
+}
+
 pub fn main() !void {
-    _ = windows.GetModuleHandleW(null);
-    std.debug.print("Hello, world!\n", .{});
+    const result = windows.EnumWindows(callback, 0);
+
+    std.log.info("{any}\n", .{result});
 }
